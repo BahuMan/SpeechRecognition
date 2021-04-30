36c16b0ff023ffb2b7f8951092e7326984ec5fe8
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2021-04-30 20:36:58 +0200

    added speech synthesis (this was easygit status)

--- SpeechRecognition/Actions/SRResponseAction.cs
+++ SpeechRecognition/Actions/SRResponseAction.cs
@@ -2,23 +2,34 @@
 using System.Xml;
 using bvba.cryingpants.SpeechRecognition.Conditions;
 using bvba.cryingpants.SpeechRecognition.Expressions;
+using System.Speech.Synthesis;
 
 namespace bvba.cryingpants.SpeechRecognition.Actions
 {
     class SRResponseAction : ISRAction
     {
+        private SpeechSynthesizer tts;
         private ISRExpression _expression;
 
         public SRResponseAction(ISRExpression expression)
         {
             _expression = expression;
+            tts = new SpeechSynthesizer();
+            tts.SetOutputToDefaultAudioDevice();
+            foreach (var v in tts.GetInstalledVoices())
+            {
+                Console.WriteLine(v.VoiceInfo.Name + "(" + v.VoiceInfo.Gender + "," + v.VoiceInfo.Age + ")" + v.VoiceInfo.Culture + " -> " + v.VoiceInfo.AdditionalInfo.Count);
+            }
         }
 
         public void PerformAction(SRStatus status, string inputstring)
         {
             //@TODO: decide whether to use console or speech synthesis
 
-            Console.WriteLine(_expression.Evaluate(status));
+            String evaluated = _expression.Evaluate(status);
+            Console.WriteLine(evaluated);
+            tts.Speak(evaluated);
+
         }
 
         public static SRResponseAction ParseXML(XmlReader xr)
